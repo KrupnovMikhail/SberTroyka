d21fb618035d532095fba4ac6ba1da6910090765
Крупнов Михаил <krupnovmn@gmail.com>
2022-04-01 19:14:31 +0300

    Initial commit

---
+++ app/src/main/java/com/krupnov/sbertroyka/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krupnov.sbertroyka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
